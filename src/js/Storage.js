fbb59dcc781c4490899785f5ae9a95e6b843897c
Mahdi Bagheri <qarjis8@gmail.com>
2023-02-07 15:33:24 +0330

    add Storage class

---
+++ src/js/Storage.js
@@ -0,0 +1,56 @@
+export default class Storage {
+  static getAllCategories() {
+    const savedCategories = JSON.parse(localStorage.getItem("category")) || [];
+    // sort Descending
+    const sortedCategories = savedCategories.sort((a, b) => {
+      return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+    });
+    return sortedCategories;
+  }
+
+  static saveCategory(categoryToSave) {
+    const savedCategories = Storage.getAllCategories();
+    const existedItem = savedCategories.find(
+      (category) => category.id === categoryToSave.id
+    );
+    if (existedItem) {
+      // edit
+      existedItem.title = categoryToSave.title;
+      existedItem.description = categoryToSave.description;
+    } else {
+      // new
+      categoryToSave.id = new Date().getTime();
+      categoryToSave.createdAt = new Date().toISOString();
+      savedCategories.push(categoryToSave);
+    }
+    localStorage.setItem("category", JSON.stringify(savedCategories));
+  }
+
+  static getAllProducts() {
+    const savedProducts = JSON.parse(localStorage.getItem("product")) || [];
+    // sort Descending
+    const sortedProducts = savedProducts.sort((a, b) => {
+      return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+    });
+    return sortedProducts;
+  }
+
+  static saveProduct(productToSave) {
+    const savedProducts = Storage.getAllProducts();
+    const existedItem = savedProducts.find(
+      (product) => product.id === productToSave.id
+    );
+    if (existedItem) {
+      // edit
+      existedItem.title = productToSave.title;
+      existedItem.quantity = productToSave.quantity;
+      existedItem.category = productToSave.category;
+    } else {
+      // new
+      existedItem.id = new Date().getTime();
+      existedItem.createdAt = new Date().toISOString();
+      savedProducts.push(productToSave);
+    }
+    localStorage.setItem("product", JSON.stringify(savedProducts));
+  }
+}
